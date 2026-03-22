e24eeefe98337426ac371226ac0e3f39e6a5693a
Angel Flores T. <aftorres02@gmail.com>
2026-03-21 22:24:16 -0500

    improve apex export

--- docs/coding-standards/apex/repo_structure/scripts/apex_install.sql
+++ docs/coding-standards/apex/repo_structure/scripts/apex_install.sql
@@ -1,23 +1,25 @@
--- Installs an APEX application
--- 
--- Parameters:
--- 1: Schema to install into
--- 2: Workspace to install into
--- 3: Application ID to run
+-- =============================================================================
+-- APEX Application Install
+-- =============================================================================
+-- Installs APEX application 100 into the target schema and workspace.
+-- Run this script from SQLcl inside the repo root.
+--
+-- Usage:
+--   @scripts/apex_install.sql
+-- =============================================================================
 
 set serveroutput on size unlimited;
-set timing off;
-set define on
+set define off;
 
-declare
+-- -----------------------------------------------------------------------------
+-- Configure the install target
+-- -----------------------------------------------------------------------------
 begin
-
-  apex_application_install.set_application_id(&3.);
-  apex_application_install.set_schema(upper('&1.'));
-  apex_application_install.set_workspace(upper('&2.'));
-  -- apex_application_install.generate_offset;
-
+  apex_application_install.set_application_id(100);
+  apex_application_install.set_schema('MY_SCHEMA');
+  apex_application_install.set_workspace('MY_WORKSPACE');
 end;
 /
 
-@../apex/f&3..sql
+-- Run the exported application file
+@../apex/f100.sql
